e5438765efb37be861fc34a97acdaddf63a0e41c
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-05-11 11:45:55 +0200

    Cucumber main method with linkerd shutdown

--- src/kotlin/no/nav/medlemskap/RunCucumber.kt
+++ src/kotlin/no/nav/medlemskap/RunCucumber.kt
@@ -1,6 +1,9 @@
 package no.nav.medlemskap
 
-import io.cucumber.core.cli.Main
+import io.cucumber.core.options.CommandlineOptionsParser
+import io.cucumber.core.options.CucumberProperties
+import io.cucumber.core.options.CucumberPropertiesParser
+import io.cucumber.core.runtime.Runtime
 import no.nav.medlemskap.client.shutdownLinkerdSidecar
 import kotlin.system.exitProcess
 
@@ -14,7 +17,40 @@ val args = arrayOf(
 
 fun main() {
     AuthenticationTest().runTests()
-    Main.main(*args) //Cucumber tests
+    val exitStatus = cucumberMainMethodWithLinkerdShutdown()
+
     shutdownLinkerdSidecar()
-    exitProcess(0)
+    exitProcess(exitStatus.toInt())
+}
+
+private fun cucumberMainMethodWithLinkerdShutdown(): Byte {
+    val propertiesFileOptions = CucumberPropertiesParser()
+        .parse(CucumberProperties.fromPropertiesFile())
+        .build()
+
+    val environmentOptions = CucumberPropertiesParser()
+        .parse(CucumberProperties.fromEnvironment())
+        .build(propertiesFileOptions)
+
+    val systemOptions = CucumberPropertiesParser()
+        .parse(CucumberProperties.fromSystemProperties())
+        .build(environmentOptions)
+
+    val runtimeOptions = CommandlineOptionsParser()
+        .parse(*args)
+        .addDefaultGlueIfAbsent()
+        .addDefaultFeaturePathIfAbsent()
+        .addDefaultFormatterIfAbsent()
+        .addDefaultSummaryPrinterIfAbsent()
+        .build(systemOptions)
+
+
+    val runtime = Runtime.builder()
+        .withRuntimeOptions(runtimeOptions)
+        .withClassLoader { Thread.currentThread().contextClassLoader }
+        .build()
+
+    runtime.run()
+    val exitStatus = runtime.exitStatus()
+    return exitStatus
 }
